7ce2337f34c4c5fc0347b9b58d3459555ff4e8b8
larryzhu <bo.z.zhu@gmail.com>
2021-06-06 14:12:09 +0800

    Add files via upload

---
+++ TTarm32.sh
@@ -0,0 +1,4 @@
+#!/bin/Bash
+#PJ小宇
+#甜糖邀请码272440
+echo 'nameserver 119.29.29.29' >/etc/resolv.conf&&cd&&wget --no-check-certificate https://raw.githubusercontent.com/ihuaer/greenhost/master/main.zip&&unzip -o main.zip -d /root >/root/null&&cd /root/TTarm&&sh TT461491.sh
